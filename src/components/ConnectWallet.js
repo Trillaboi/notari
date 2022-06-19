570d6bdfa2c990d6870a130e0683ec871dd13338
Chisom Ikejiani <trillaboi@gmail.com>
2022-06-19 03:03:32 -0400

    Revert "added simple api"
    This reverts commit 591a47429ba37346dff8d9fbcaec9faaa5e60ad9.

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -6,7 +6,6 @@ export function ConnectWallet() {
     const connect = useConnect();
     return (
       <button
-      className="mb-16 Button px-2 py-1 bg-purple-600 text-purple-50 font-semibold rounded-md"
         onClick={() => connect.doAuth()}
       >
         Connect Wallet
